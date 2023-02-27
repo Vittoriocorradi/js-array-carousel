80a660444e9e60232e24eb89d4bfa4f40c5bf2ea
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-27 19:37:30 +0100

    Stilizzazione

--- js/main.js
+++ js/main.js
@@ -2,23 +2,28 @@
 
 // Lista immagini
 const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+const titles = ['Spider-Man', 'Ratchet & Clank', 'Fortnite', 'Stray', 'Marvel: Avengers'];
 // Variabili
 let active = 0;
 const slider = document.querySelector('.slider');
 const thumbNail = document.querySelector('.thumbnails');
+const titleGroup = document.querySelector('.titles');
 
 // Ciclo per immagini dinamiche
 for (let i = 0; i < imgs.length; i++) {
     slider.innerHTML += `<div class="item"><img src="img/${imgs[i]}" alt="Immagine ${i + 1}"></div>`;
     thumbNail.innerHTML += `<div class="thumbnail shadow" id="${i + 1}"><img src="img/${imgs[i]}" alt="Immagine ${i + 1}"></div>`;
+    titleGroup.innerHTML += `<div class="game-title">${titles[i]}</div>`;
 }
 
 // Lista elementi nascosti
 const items = document.querySelectorAll('.item');
+const gameTitles = document.querySelectorAll('.game-title');
 // Lista elementi oscurati
 const thumbNails = document.querySelectorAll('.thumbnail');
 // Aggiunta classe a un elemento per renderlo visibile
 items[active].classList.add('show');
+gameTitles[active].classList.add('show');
 // Rimossa classe a un elemento per renderlo non oscurato
 thumbNails[active].classList.remove('shadow');
 
@@ -31,15 +36,19 @@ previous.addEventListener('click',
 function() {
     if (active > 0) {
         items[active].classList.remove('show');
+        gameTitles[active].classList.remove('show');
         thumbNails[active].classList.add('shadow')
         active--;
         items[active].classList.add('show');
+        gameTitles[active].classList.add('show');
         thumbNails[active].classList.remove('shadow')
     } else {
         items[active].classList.remove('show');
+        gameTitles[active].classList.remove('show');
         thumbNails[active].classList.add('shadow')
         active = imgs.length - 1;
         items[active].classList.add('show');
+        gameTitles[active].classList.add('show');
         thumbNails[active].classList.remove('shadow')
     }
 }
@@ -50,15 +59,19 @@ next.addEventListener('click',
 function() {
     if (active < imgs.length - 1) {
         items[active].classList.remove('show');
+        gameTitles[active].classList.remove('show');
         thumbNails[active].classList.add('shadow')
         active++;
         items[active].classList.add('show');
+        gameTitles[active].classList.add('show');
         thumbNails[active].classList.remove('shadow')
     } else {
         items[active].classList.remove('show');
+        gameTitles[active].classList.remove('show');
         thumbNails[active].classList.add('shadow')
         active = 0;
         items[active].classList.add('show');
+        gameTitles[active].classList.add('show');
         thumbNails[active].classList.remove('shadow')
     }
 }
@@ -122,9 +135,11 @@ thumbNail.addEventListener('click', function(e) {
     
     if (mini !== "") {
     items[active].classList.remove('show');
+    gameTitles[active].classList.remove('show');
     thumbNails[active].classList.add('shadow')
     active = mini - 1;
     items[active].classList.add('show');
+    gameTitles[active].classList.add('show');
     thumbNails[active].classList.remove('shadow')
     console.log(mini);}
 }, false);
