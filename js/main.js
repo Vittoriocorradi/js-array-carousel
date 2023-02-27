b2f0f296f7ddd22456464af2c44db3e49bb1e8e8
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-02-27 15:52:46 +0100

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// Lista immagini
+const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+// Variabili
+let active = 0;
+const slider = document.querySelector('.slider');
+
+// Ciclo per immagini dinamiche
+for (let i = 0; i < imgs.length; i++) {
+    slider.innerHTML += `<div class="item"><img src="img/${imgs[i]}" alt="Immagine ${i + 1}"></div>`;
+}
+
+// Lista elementi nascosti
+const items = document.querySelectorAll('.item');
+// Aggiunta classe a un elemento per renderlo visibile
+items[active].classList.add('show');
